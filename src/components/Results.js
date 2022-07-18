9308eabda74bc811c70a13e340c0a2ccfab74972
stvnlee890 <stvnlee890@gmail.com>
2022-07-17 23:05:05 -0700

    able to set id for each images and target them with onclick

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,16 +7,19 @@ function Results({ images }) {
     return <h2>no images found!</h2>
   }
   // console.log(images.results[0].thumb)
+  // Inside the onClick function is where you route the viewers to the sub component
   const onClick = (event) => {
-    if(images.id === 1993487){
+    if(event.target.id === '1993487'){
       console.log(true)
+    }else{
+      console.log(false)
     }
-    console.log(event.target)
+    console.log(event.target.id)
   }
   return (
     <div className='gallery'>
     {images.map((image) => (
-      <div key={image.id} className='image-resutls' >
+      <div key={image.id} className='image-results' >
         <img onClick={onClick} src={image.thumb} id={image.id}/>
       </div>
     ))}
