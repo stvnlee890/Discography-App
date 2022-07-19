90f758860ff5ca50ecfe6a3292f95009b54cd489
stvnlee890 <stvnlee890@gmail.com>
2022-07-19 13:28:27 -0700

    progressing with artist component

--- src/components/ArtistInfo/ArtistProfile.js
+++ src/components/ArtistInfo/ArtistProfile.js
@@ -1,13 +1,28 @@
 import '../../index.css'
+import React from 'react'
 function ArtistProfile({ artistInformation }) {
   return(
     <div>
       <img className='profile-image' src={artistInformation.images[0].uri} alt='profile-img'/>
+      <h3>
+        Aliases
+      </h3>
+        {artistInformation.aliases.map(aliases => (
+          <div>{aliases.name}</div>
+        ))}
+      <h3>
+        Bio
+      </h3>
       <div className='bio' dangerouslySetInnerHTML={{__html: `<p>${artistInformation.profile_html}</p>`}} />
-      
-        {artistInformation.map((nameVar)=>(
-          <p>{nameVar.namevariation}</p>
-        ) )}
+      <h3>
+        URLS
+      </h3>
+      <p>
+        {artistInformation.urls.map(nameVar=>(
+          <p>{nameVar}</p>
+          ))}
+      </p>
+ 
       
     </div>
   )
